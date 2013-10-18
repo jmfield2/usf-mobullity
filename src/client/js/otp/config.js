45ff59681e7aaecbbb998268f4a7f9ee5e3b2975
TNWolf <tnwolf@mail.usf.edu>
2013-10-18 10:27:20 -0400

    Geocoding now set up to work.

--- src/client/js/otp/config.js
+++ src/client/js/otp/config.js
@@ -78,8 +78,7 @@ otp.config = {
             tileUrl: 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png',
             subdomains : ['otile1','otile2','otile3','otile4'],
             attribution : 'Data, imagery and map information provided by <a href="http://open.mapquest.com" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.'
-        },           
-        
+        },   
     ],
     
 
@@ -91,7 +90,7 @@ otp.config = {
     
     //set init lat lng in map.js with geolocation
     geoLocation: true,
-    locatorSwitch: true,
+    locatorSwitch: false,
     initLatLng : new L.LatLng(28.058499, -82.416945), 
     initZoom : 14,
     minZoom : 8,
@@ -157,17 +156,18 @@ otp.config = {
      */
 
     geocoders : [
+<<<<<<< HEAD:src/client/js/otp/config.js
         {
             name: 'OTP built-in geocoder',
             className: 'otp.core.GeocoderBuiltin'
             // URL and query parameter do not need to be set for built-in geocoder.
         }
- //               {
- //               	 name: "Google Geocoder",
- //               	 className: "otp.core.SOLRGeocoder",
- //               	 url: "/opentripplanner-geocoder/geocode",
- //               	 addressParam: "address",
-                 }
+  //              {
+  //              	 name: "Geocoder",
+  //              	 className: "otp.core.Geocoder",
+  //              	 url: "/opentripplanner-geocoder/geocode",
+  //              	 addressParam: "address",
+  //               }
     ],
 
     
