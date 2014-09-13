4e04a1697c9deff7455117db03756ed508bbaaf1
Marko Burjek <s0mebody.slo@gmail.com>
2014-09-13 18:21:53 +0200

    Bike and Wheelchair trip capability are shown in trip Widget
    TODO: Show this in print view

--- otp-core/src/main/java/org/opentripplanner/api/ws/PlanGenerator.java
+++ otp-core/src/main/java/org/opentripplanner/api/ws/PlanGenerator.java
@@ -614,6 +614,8 @@ public class PlanGenerator {
             leg.tripId = trip.getId().getId();
             leg.tripShortName = trip.getTripShortName();
             leg.tripBlockId = trip.getBlockId();
+            leg.wheelchairAccessible = trip.getWheelchairAccessible();
+            leg.bikesAllowed = trip.getBikesAllowed();
 
             if (serviceDay != null) {
                 leg.serviceDate = serviceDay.getServiceDate().getAsString();
