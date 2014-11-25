10c9ca3d5ac34bad1796be6bcc5c25f842ed349d
jmfield2 <jmfield2@mail.usf.edu>
2014-11-25 13:14:01 -0500

    add bbox to bike_lanes API @TODO replace this with polyline string generated one time at startup/graph reload

--- src/main/java/org/opentripplanner/api/resource/BikeLanes.java
+++ src/main/java/org/opentripplanner/api/resource/BikeLanes.java
@@ -18,10 +18,13 @@ import javax.xml.bind.annotation.XmlRootElement;
 import lombok.Setter;
 
 import org.opentripplanner.routing.edgetype.PlainStreetEdge;
+import org.opentripplanner.routing.edgetype.StreetEdge;
 import org.opentripplanner.routing.edgetype.StreetTraversalPermission;
 import org.opentripplanner.routing.graph.Edge;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.services.GraphService;
+
+import com.vividsolutions.jts.geom.Envelope;
 
 @Path("/routers/{routerId}/bike_lanes")
 @XmlRootElement
@@ -33,13 +36,25 @@ public class BikeLanes {
 
 		@GET
 		@Produces({ MediaType.APPLICATION_JSON })
-		public ArrayList<String> getBikeLanes(@PathParam("routerId") String routerId) {
+		public ArrayList<String> getBikeLanes(
+				@PathParam("routerId") String routerId,
+	            @QueryParam("lowerLeft") String lowerLeft,
+	            @QueryParam("upperRight") String upperRight) {
+			
 			ArrayList<String> tmp = new ArrayList<String>();
-		
+
+	        Envelope bboxEnvelope;
+	        if (lowerLeft != null) {
+	            bboxEnvelope = getEnvelope(lowerLeft, upperRight);
+	        } else {
+	            bboxEnvelope = new Envelope(-180,180,-90,90); 
+	        }
+			
 			Graph g = graphService.getGraph(routerId);
+			Collection<StreetEdge> edges = g.streetIndex.getEdgesForEnvelope(bboxEnvelope);
 			if (g == null) return null;
 			
-			for (Edge e : g.getEdges()) {					
+			for (Edge e : edges) {					
 				if (e.getClass() == PlainStreetEdge.class) {
 					if (((PlainStreetEdge) e).getPermission().allows(StreetTraversalPermission.BICYCLE_LANE)) {
 						tmp.add(String.format("{\"lat1\":\"%f\", \"lon1\":\"%f\", \"lat2\":\"%f\", \"lon2\":\"%f\"}", e.getFromVertex().getLat(), e.getFromVertex().getLon(), e.getToVertex().getLat(), e.getToVertex().getLon()));	
@@ -50,5 +65,16 @@ public class BikeLanes {
 			
 			return tmp;
 		}
-	
+
+	    /** Envelopes are in latitude, longitude format */
+	    public static Envelope getEnvelope(String lowerLeft, String upperRight) {
+	        String[] lowerLeftParts = lowerLeft.split(",");
+	        String[] upperRightParts = upperRight.split(",");
+
+	        Envelope envelope = new Envelope(Double.parseDouble(lowerLeftParts[1]),
+	                Double.parseDouble(upperRightParts[1]), Double.parseDouble(lowerLeftParts[0]),
+	                Double.parseDouble(upperRightParts[0]));
+	        return envelope;
+	    }
+		
 }
